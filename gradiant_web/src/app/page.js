807def8041053563b9a992381efd730061996d22
Raúl Mouzo <raul.mouzo@udc.es>
2024-02-17 18:40:53 +0100

    Add example download button

--- gradiant_web/src/app/page.js
+++ gradiant_web/src/app/page.js
@@ -1,6 +1,15 @@
 'use client';
 
 import { DragAndDrop } from "@/app/components/DragAndDrop";
+import { FiDownload } from "react-icons/fi";
+
+
+
+const handleDownloadCSV = async () => {
+ 
+};
+
+
 
 export default function Home() {
   return (
@@ -17,6 +26,17 @@ export default function Home() {
               </span>
           </h1>
           <DragAndDrop/>
+          
+          <button
+            onClick={handleDownloadCSV}
+            className="flex items-center gap-2 text-gray-800 bg-gray-200 dark:bg-gray-700 dark:text-gray-100 px-4 py-2 rounded-md shadow-md hover:shadow-lg transition duration-300"
+            style={{ marginTop: '20px' }}
+          >
+            <FiDownload className="text-xl" />
+            Download CSV Example
+          </button>
+
+
         </div>
       </div>
     </main>
