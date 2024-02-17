17cca6d7820294b6e13be34417338a7a3dfa02d3
FerLS <fernando.alvarezr@udc.es>
2024-02-17 20:48:25 +0100

    ineedlittlebitofsandwechsa

--- gradiant_web/src/app/components/PriceCard.jsx
+++ gradiant_web/src/app/components/PriceCard.jsx
@@ -3,26 +3,26 @@ import React from 'react';
 
 function PriceCard({ price, title }) {
   return (
-    <div className='border-white border-2 rounded-lg h-[350px] w-[350px]'>
-      <h3 className='text-[30px] font-sans font-semibold mx-3 text-center'>
+    <div className='border-white border-[5px] rounded-lg h-[350px] w-[350px]'>
+      <h3 className='text-[30px] font-sans font-semibold m-5 text-center'>
         {title}
       </h3>
-      <div className='text-[20px] font-sans font-semibold mx-3 text-start mt-3'>
+      <div className='text-[20px] font-sans font-semibold m-5 text-start mt-5'>
         Price:
       </div>
-      <div className='text-[30px] text-white font-sans font-semibold mx-3 mt-1'>
+      <div className='text-[30px] text-white font-sans font-semibold mx-5 mt-1'>
         {price?.price} €/kWh
       </div>
-      <div className='text-[20px] font-sans font-semibold mx-3 text-start mt-3'>
+      <div className='text-[20px] font-sans font-semibold mx-5 text-start mt-3'>
         Date:
       </div>
-      <div className='text-[30px] text-white font-sans font-semibold mx-3 mt-1'>
+      <div className='text-[30px] text-white font-sans font-semibold mx-5 mt-1'>
         {price?.date}
       </div>
-      <div className='text-[20px] font-sans font-semibold mx-3 text-start mt-3'>
+      <div className='text-[20px] font-sans font-semibold mx-5 text-start mt-5'>
         Hour:
       </div>
-      <div className='text-[30px] text-white font-sans font-semibold mx-3 mt-1'>
+      <div className='text-[30px] text-white font-sans font-semibold mx-5 mt-1'>
         {price?.hour}
       </div>
     </div>
